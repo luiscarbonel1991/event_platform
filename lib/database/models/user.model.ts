c7de2a5744c1f8af42a0c946e4a2e1e301c2f9e4
luiscarbonel1991 <devhighlevel@gmail.com>
2024-05-28 15:05:00 -0500

    Update user model

--- lib/database/models/user.model.ts
+++ lib/database/models/user.model.ts
@@ -4,9 +4,9 @@ const UserSchema = new Schema({
   clerkId: { type: String, required: true, unique: true },
   email: { type: String, required: true, unique: true },
   username: { type: String, required: true, unique: true },
-  firstName: { type: String, required: true },
-  lastName: {type: String, required: true },
-  photo: { type: String, required: true },
+  firstName: { type: String, default: ''},
+  lastName: {type: String, default: '' },
+  photo: { type: String, default: '' },
 })
 
 const User = models.User || model('User', UserSchema);
